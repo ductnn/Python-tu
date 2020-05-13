3c372f9f8abd5f1f640d384258c9193c6998edc8
DuctnNetwork <trannhuduc531998@gmail.com>
2020-05-13 23:47:41 +0700

    Update

--- algoritms/hackerRank/Easy/birthday_chocolate.py
+++ algoritms/hackerRank/Easy/birthday_chocolate.py
@@ -1,9 +1,4 @@
-#!/bin/python3
-
 import math
-import os
-import random
-import re
 import sys
 
 # Complete the birthday function below.
@@ -21,21 +16,17 @@ def birthday(s, d, m):
             break
     return ans
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input().strip())
+n = int(input().strip())
 
-    s = list(map(int, input().rstrip().split()))
+s = list(map(int, input().rstrip().split()))
 
-    dm = input().rstrip().split()
+dm = input().rstrip().split()
 
-    d = int(dm[0])
+d = int(dm[0])
 
-    m = int(dm[1])
+m = int(dm[1])
 
-    result = birthday(s, d, m)
+result = birthday(s, d, m)
 
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+print(str(result) + '\n')
