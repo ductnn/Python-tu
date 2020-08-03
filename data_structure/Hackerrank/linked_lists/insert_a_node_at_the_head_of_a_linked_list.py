b6a144a5a0ad3a41be63e62fc7a2a24f42cfb6d0
DuctnNetwork <trannhuduc531998@gmail.com>
2020-08-03 23:19:28 +0700

    Hackerrank Easy DataStructure Insert a node at the head of a linked list

---
+++ data_structure/Hackerrank/linked_lists/insert_a_node_at_the_head_of_a_linked_list.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        print(str(node.data))
+
+        node = node.next
+
+        if node:
+            print(sep)
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+class Node(object): 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+def insertNodeAtHead(head, data):
+    if head is None:
+        head = Node(data)
+        return head
+    else:
+        buf = head     
+        head = Node(data)
+        head.next = buf      
+        return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    
+    print_singly_linked_list(llist.head, '\n')
+    print('\n')
+
