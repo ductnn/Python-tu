f61bf37d1ab56553e9d43c0edc8085b80df3d27f
DuctnNetwork <trannhuduc531998@gmail.com>
2020-08-04 23:09:04 +0700

    Hackerrank Easy DataStructure Delete a Node

---
+++ data_structure/Hackerrank/linked_lists/delete_a_node.py
@@ -0,0 +1,80 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        print(str(node.data))
+
+        node = node.next
+
+        if node:
+            print(sep)
+
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+    if position == 0:
+        head = head.next
+    else:
+        node = head
+        for _ in range(position-1):
+            node = node.next
+        node.next = node.next.next
+    
+    return head
+
+if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1)
+    print('\n')
+
