293073d54f9917071ac9e11f4aa8d0b7599e3cc4
ductnn <trannhuduc531998@gmail.com>
2021-03-25 17:21:08 +0700

    Data Structure - BTS insertion

---
+++ data_structure/Hackerrank/Easy/BTS_insertion.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, info):
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+def preOrder(root):
+    if root == None:
+        return
+    print (root.info, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+    
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+#Node is defined as
+#self.left (the left child of the node)
+#self.right (the right child of the node)
+#self.info (the value of the node)
+
+    def insert(self, val):
+        #Enter you code here.
+        new_node = Node(val)
+
+        # Case 1: root is NULL
+        if self.root is None:
+            self.root = new_node
+            return
+
+        current = self.root
+
+        while current:
+            if val < current.info:
+                if current.left is None:
+                    current.left = new_node
+                    return
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = new_node
+                    return
+                current = current.right
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.insert(arr[i])
+
+preOrder(tree.root)
