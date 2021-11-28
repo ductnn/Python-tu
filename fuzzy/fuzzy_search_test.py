9623a57823647ab067807754941de640b89e2f9c
ductnn <trannhuduc531998@gmail.com>
2021-11-28 20:50:38 +0700

    Add levenshtein distance

--- fuzzy/fuzzy_search_test.py
+++ fuzzy/fuzzy_search_test.py
@@ -1,6 +1,34 @@
 from exception_test import FoundedException
 
 
+def swap(a, b):
+    a, b = b, a
+
+
+# levenshtein distance
+def levenshtein_distance(str1, str2):
+    str1_len = len(str1)
+    str2_len = len(str2)
+
+    if str1_len > str2_len:
+        swap(str1_len, str2_len)
+
+    distances = range(str1_len + 1)
+
+    for i2, c2 in enumerate(str2):
+        distances_ = [i2 + 1]
+        for i1, c1 in enumerate(str1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(
+                    1 + min((distances[i1], distances[i1 + 1], distances_[-1]))
+                )
+        distances = distances_
+
+    return distances[-1]
+
+
 def fuzzy_search(needle, haystack):
     needle_len = len(needle)
     haystack_len = len(haystack)
@@ -23,3 +51,6 @@ def fuzzy_search(needle, haystack):
             pass
 
     return True
+
+
+# https://github.com/imalic3/levenshtein-distance-python/blob/master/levenshtein_distance.py
